128e49eff8ce65fa2ed021f9e1cfc35d70b041c7
Maxi717 <83463466+Maxi717@users.noreply.github.com>
2022-11-08 22:03:23 -0300

    3 files modified, 1 added

--- src/app/components/articulos/articulos.component.ts
+++ src/app/components/articulos/articulos.component.ts
@@ -4,7 +4,8 @@ import { ArticuloFamilia } from '../../models/articulo-familia';
 import { MockArticulosService } from '../../services/mock-articulos.service';
 import { MockArticulosFamiliasService } from '../../services/mock-articulos-familias.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { ArticulosService } from '../../services/articulos.service';
+import { ArticulosFamiliasService } from '../../services/articulos-familias.service';
 
 @Component({
   selector: 'app-articulos',
@@ -49,16 +50,17 @@ export class ArticulosComponent implements OnInit {
     Nombre: new FormControl(''),
     Precio: new FormControl(null),
     Stock: new FormControl(null),
-    CodigoDeBarra: new FormControl (''),
+    CodigoDeBarra: new FormControl(''),
     IdArticuloFamilia: new FormControl(''),
     FechaAlta: new FormControl(''),
     Activo: new FormControl(true),
   });
 
-
   constructor(
-    private articulosService: MockArticulosService,
-    private articulosFamiliasService: MockArticulosFamiliasService
+    //private articulosService: MockArticulosService,
+    //private articulosFamiliasService: MockArticulosFamiliasService
+    private articulosService: ArticulosService,
+    private articulosFamiliasService: ArticulosFamiliasService
   ) {}
 
   ngOnInit() {
@@ -72,15 +74,18 @@ export class ArticulosComponent implements OnInit {
   }
 
   Agregar() {
-    this.AccionABMC = "A";
+    this.AccionABMC = 'A';
     this.FormRegistro.reset({ Activo: true, IdArticulo: 0 });
   }
-
 
   // Buscar segun los filtros, establecidos en FormRegistro
   Buscar() {
     this.articulosService
-      .get(this.FormBusqueda.value.Nombre, this.FormBusqueda.value.Activo, this.Pagina)
+      .get(
+        this.FormBusqueda.value.Nombre,
+        this.FormBusqueda.value.Activo,
+        this.Pagina
+      )
       .subscribe((res: any) => {
         this.Items = res.Items;
         this.RegistrosTotal = res.RegistrosTotal;
@@ -88,23 +93,20 @@ export class ArticulosComponent implements OnInit {
   }
 
   // Obtengo un registro especifico según el Id
-  BuscarPorId(Item:Articulo, AccionABMC:string ) {
- 
+  BuscarPorId(Item: Articulo, AccionABMC: string) {
     window.scroll(0, 0); // ir al incio del scroll
- 
+
     this.articulosService.getById(Item.IdArticulo).subscribe((res: any) => {
-  
-      const itemCopy = { ...res };  // hacemos copia para no modificar el array original del mock
-      
+      const itemCopy = { ...res }; // hacemos copia para no modificar el array original del mock
+
       //formatear fecha de  ISO 8601 a string dd/MM/yyyy
-      var arrFecha = itemCopy.FechaAlta.substr(0, 10).split("-");
-      itemCopy.FechaAlta = arrFecha[2] + "/" + arrFecha[1] + "/" + arrFecha[0];
- 
+      var arrFecha = itemCopy.FechaAlta.substr(0, 10).split('-');
+      itemCopy.FechaAlta = arrFecha[2] + '/' + arrFecha[1] + '/' + arrFecha[0];
+
       this.FormRegistro.patchValue(itemCopy);
       this.AccionABMC = AccionABMC;
     });
   }
-
 
   Consultar(Item: Articulo) {
     this.BuscarPorId(Item, 'C');
@@ -121,22 +123,29 @@ export class ArticulosComponent implements OnInit {
 
   // grabar tanto altas como modificaciones
   Grabar() {
- 
     //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
-    const itemCopy = { ...this.FormRegistro.value };
- 
+    const itemCopy: Articulo = {
+      IdArticulo: this.FormRegistro.value.IdArticulo,
+      Nombre: this.FormRegistro.value.Nombre,
+      Precio: this.FormRegistro.value.Precio,
+      Stock: this.FormRegistro.value.Stock,
+      CodigoDeBarra: this.FormRegistro.value.CodigoDeBarra,
+      IdArticuloFamilia: parseInt(this.FormRegistro.value.IdArticuloFamilia),
+      FechaAlta: this.FormRegistro.value.FechaAlta,
+      Activo: this.FormRegistro.value.Activo,
+    };
+
     //convertir fecha de string dd/MM/yyyy a ISO para que la entienda webapi
-    var arrFecha = itemCopy.FechaAlta.substr(0, 10).split("/");
+    var arrFecha = itemCopy.FechaAlta.substr(0, 10).split('/');
     if (arrFecha.length == 3)
-      itemCopy.FechaAlta = 
-          new Date(
-            arrFecha[2],
-            arrFecha[1] - 1,
-            arrFecha[0]
-          ).toISOString();
- 
+      itemCopy.FechaAlta = new Date(
+        parseInt(arrFecha[2]),
+        parseInt(arrFecha[1]) - 1,
+        parseInt(arrFecha[0])
+      ).toISOString();
+
     // agregar post
-    if (this.AccionABMC == "A") {
+    if (this.AccionABMC == 'A') {
       this.articulosService.post(itemCopy).subscribe((res: any) => {
         this.Volver();
         alert('Registro agregado correctamente.');
@@ -154,22 +163,18 @@ export class ArticulosComponent implements OnInit {
     }
   }
 
-
-  ActivarDesactivar(Item : Articulo) {
+  ActivarDesactivar(Item: Articulo) {
     var resp = confirm(
-      "Esta seguro de " +
-        (Item.Activo ? "desactivar" : "activar") +
-        " este registro?");
-    if (resp === true)
-    {
-     this.articulosService  
-          .delete(Item.IdArticulo)
-          .subscribe((res: any) => 
-            this.Buscar()
-          );
+      'Esta seguro de ' +
+        (Item.Activo ? 'desactivar' : 'activar') +
+        ' este registro?'
+    );
+    if (resp === true) {
+      this.articulosService
+        .delete(Item.IdArticulo)
+        .subscribe((res: any) => this.Buscar());
     }
   }
-
 
   // Volver desde Agregar/Modificar
   Volver() {
@@ -179,9 +184,9 @@ export class ArticulosComponent implements OnInit {
   ImprimirListado() {
     alert('Sin desarrollar...');
   }
-}
 
-GetArticuloFamiliaNombre(Id:number) {
-  let Nombre = this.Familias.find(x => x.IdArticuloFamilia === Id)?.Nombre;
-  return Nombre;
+  GetArticuloFamiliaNombre(Id: number) {
+    let Nombre = this.Familias.find((x) => x.IdArticuloFamilia === Id)?.Nombre;
+    return Nombre;
+  }
 }
